41e7af266649b9b4177ea622a4839982ef36b3ff
INSETEAM8C <INSETEAM8C@gmail.com>
2016-01-03 18:24:19 +0000

    pre-merge corrections for workload 1

--- gitTest/test1.java
+++ gitTest/test1.java
@@ -5,7 +5,9 @@
 public class test1{
   public static void main(String[] args){
     //Adds some strings for printing
-    System.out.println("Hello world!");
+    System.out.println("Goodbye world!");
     System.out.println("I think you've added something which hasn't been comitted...");
+    //Added work for workload 1
+    System.out.println("Workload 1 completed");
   }
 }
