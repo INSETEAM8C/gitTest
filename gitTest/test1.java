aea80fcaa6ba0818a1d27bc8647afa89006a0066
INSETEAM8C <INSETEAM8C@gmail.com>
2016-01-03 17:58:56 +0000

    Added some relevant comments for clarity

--- gitTest/test1.java
+++ gitTest/test1.java
@@ -1,5 +1,10 @@
+//@Author: Jake Hedges
+//@Date: 03/01/2016
+//A piece of code which is used to test GitHub
+
 public class test1{
   public static void main(String[] args){
+    //Adds some strings for printing
     System.out.println("Hello world!");
     System.out.println("I think you've added something which hasn't been comitted...");
   }
