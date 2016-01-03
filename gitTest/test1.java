3e34dd46c0f9b550cfa54a4225c96ace8231fcd7
INSETEAM8C <INSETEAM8C@gmail.com>
2016-01-03 17:46:46 +0000

    added one more 'system.out.println' status as a secondary test (for modification)

--- gitTest/test1.java
+++ gitTest/test1.java
@@ -1,5 +1,6 @@
 public class test1{
   public static void main(String[] args){
     System.out.println("Hello world!");
+    System.out.println("I think you've added something which hasn't been comitted...");
   }
 }
